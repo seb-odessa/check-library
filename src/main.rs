f0fd4b986ef5395dbd793284bd4c7969c6883967
Pavel Kolievatykh <pavlo.kolievatykh@dxc.com>
2024-07-28 22:27:47 +0300

    Added sorting and additional routine for inpx

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use md5;
+use memmap2::Mmap;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs::{self, File};
-use std::io::{self, BufRead, Read, Write};
+use std::io::{self, BufRead, Write};
 use std::path::Path;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Md5Cache {
-    md5map: HashMap<String, String>,
+    md5map: BTreeMap<String, String>,
 }
 
 impl Md5Cache {
@@ -20,7 +21,7 @@ impl Md5Cache {
             Ok(cache)
         } else {
             Ok(Md5Cache {
-                md5map: HashMap::new(),
+                md5map: BTreeMap::new(),
             })
         }
     }
@@ -34,18 +35,21 @@ impl Md5Cache {
 }
 
 fn calculate_md5(file_path: &Path) -> Result<String, io::Error> {
-    let mut file = File::open(file_path)?;
-    let mut hasher = md5::Context::new();
-    let mut buffer = [0; 1024 * 1024]; // 1 Мб
+    let file = File::open(file_path)?;
     let mut count = 1;
-    let step = 100;
+    let step = 30;
     let spaces = ' '.to_string().repeat(step);
-    loop {
-        let bytes_read = file.read(&mut buffer)?;
-        if bytes_read == 0 {
-            break;
-        }
-        hasher.consume(&buffer[..bytes_read]);
+    let window_size: usize = 10 * 1024 * 1024;
+
+    let mmap = unsafe { Mmap::map(&file)? };
+
+    let mut offset = 0;
+    let mut hasher = md5::Context::new();
+    while offset < mmap.len() {
+        let end = std::cmp::min(offset + window_size, mmap.len());
+        let window = &mmap[offset..end];
+        hasher.consume(window);
+
         print!(".");
         if 0 == count % step {
             write!(io::stdout(), "\x1b[{step}D")?;
@@ -55,36 +59,49 @@ fn calculate_md5(file_path: &Path) -> Result<String, io::Error> {
         io::stdout().flush()?;
         count += 1;
 
+        offset += window_size;
     }
 
     let result = hasher.compute();
     Ok(format!("{:x}", result))
 }
 
-fn load_md5map(md5sum_path: &Path) -> Result<HashMap<String, String>, io::Error> {
+fn load_md5map(
+    root: &Path,
+    md5sum_path: &Path,
+    regex: &Regex,
+) -> Result<BTreeMap<String, String>, io::Error> {
     let file = File::open(md5sum_path)?;
     let reader = io::BufReader::new(file);
-    let mut md5map = HashMap::new();
+    let mut md5map = BTreeMap::new();
     for line in reader.lines() {
         let line = line?;
         let parts: Vec<&str> = line.split_whitespace().collect();
         if parts.len() == 2 {
             let md5_sum = parts[0].to_string();
             let file_path = parts[1].replace('*', "/").replace('\\', "/");
-            md5map.insert(file_path, md5_sum);
+
+            if let Ok(file_name) = Path::new(&file_path).strip_prefix(root) {
+                if let Some(file_name) = file_name.to_str() {
+                    if regex.is_match(&file_name) {
+                        md5map.insert(file_path, md5_sum);
+                    } else {
+
+                    }
+                }
+            } else {
+                if file_path.ends_with("inpx") {
+                    let inpx = root.display().to_string() + &file_path;
+                    md5map.insert(inpx, md5_sum);
+                }
+            }
         }
     }
     Ok(md5map)
 }
 
-fn check_integrity(
-    root: &Path,
-    md5sum: &Path,
-    cache_file: &Path,
-    regex: &Regex,
-) -> Result<(), io::Error> {
-    let mut cache = Md5Cache::load(cache_file)?;
-    let md5map = load_md5map(md5sum)?;
+fn get_filtered_and_sorted(root: &Path, regex: &Regex) -> Result<Vec<String>, io::Error> {
+    let mut files = Vec::new();
 
     for entry in fs::read_dir(root)? {
         let path = entry?.path();
@@ -97,34 +114,63 @@ fn check_integrity(
                 .replace('\\', "/");
 
             if regex.is_match(&relative_path) {
-                let full_path = path.display().to_string();
-                let cached_md5 = cache.md5map.get(&full_path).cloned();
-
-                if let Some(expected_md5) = md5map.get(&full_path) {
-                    print!("{full_path}.");
-                    io::stdout().flush()?;
-
-                    let calculated_md5 = match cached_md5 {
-                        Some(cached_md5) => cached_md5,
-                        None => {
-                            let md5 = calculate_md5(&path)?;
-                            cache.md5map.insert(full_path.clone(), md5.clone());
-                            cache.save(cache_file)?;
-                            md5
-                        }
-                    };
-
-                    if &calculated_md5 != expected_md5 {
-                        println!(".FAIL. Expected: {expected_md5}, Actual: {calculated_md5}");
-                    } else {
-                        println!(".OK.");
-                    }
-                } else {
-                    println!("{full_path} absent in {}.", md5sum.display());
-                }
+                files.push(path.display().to_string());
             }
         }
     }
+    files.sort();
+    Ok(files)
+}
+
+fn find_missing_files(expected: &BTreeMap<String, String>, calculated: &BTreeMap<String, String>) {
+    for key in expected.keys() {
+        if !calculated.contains_key(key) {
+            println!("{key} is missed or was not handled");
+        }
+    }
+}
+
+fn check_integrity(
+    root: &Path,
+    md5sum: &Path,
+    cache_file: &Path,
+    regex: &Regex,
+) -> Result<(), io::Error> {
+    let mut cache = Md5Cache::load(cache_file)?;
+    let md5map = load_md5map(root, md5sum, regex)?;
+    let filtered = get_filtered_and_sorted(root, regex)?;
+
+    for file in filtered.into_iter() {
+        let cached_md5 = cache.md5map.get(&file).cloned();
+
+        if let Some(expected_md5) = md5map.get(&file) {
+            print!("{file}.");
+            io::stdout().flush()?;
+
+            let calculated_md5 = match cached_md5 {
+                Some(cached_md5) => cached_md5,
+                None => {
+                    let path = Path::new(&file);
+                    let md5 = calculate_md5(&path)?;
+                    cache.md5map.insert(file.clone(), md5.clone());
+                    cache.save(cache_file)?;
+                    md5
+                }
+            };
+
+            if &calculated_md5 != expected_md5 {
+                println!(".FAIL.");
+                println!("Expected: {expected_md5}, Actual: {calculated_md5}");
+                cache.md5map.remove(&file);
+                cache.save(cache_file)?;
+            } else {
+                println!(".OK.");
+            }
+        } else {
+            println!("{file} absent in {}.", md5sum.display());
+        }
+    }
+    find_missing_files(&md5map, &cache.md5map);
     Ok(())
 }
 
@@ -132,7 +178,7 @@ fn main() -> Result<(), io::Error> {
     let root = Path::new("/lib.rus.ec");
     let md5sum = Path::new("/lib.rus.ec/librusec_local.md5");
     let cache = Path::new("/lib.rus.ec/md5_cache.json");
-    let regex = Regex::new(r"^fb2-\d+-\d+(_lost)?\.zip$").unwrap();
+    let regex = Regex::new(r"^fb2-\d+-\d+(_lost)?\.zip$|^librusec_local_fb2\.inpx$").unwrap();
     check_integrity(root, md5sum, cache, &regex)?;
     Ok(())
 }
